b21dd9ac521f053a50b986644f185c40092014ee
Eddie The Engineer <eddietheengineer@users.noreply.github.com>
2026-09-18 23:19:41 -0400

    fix(modem): add USE_MODEM define (network/util.h hook requires it)

--- components/modem/__init__.py
+++ components/modem/__init__.py
@@ -43,6 +43,7 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
+    cg.add_define("USE_MODEM")
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
